bc8609fe2cbf585c9282ea29d0fc5cb7e6210594
tasajo <77131055+tasajo@users.noreply.github.com>
2021-01-25 11:03:14 -0800

    indents

--- balancedstringsplit.py
+++ balancedstringsplit.py
@@ -26,14 +26,14 @@ Example 1:
 
 Input: s = "RLRRLLRLRL"
 Output: 4
-Explanation: s can be split into "RL", "RRLL", "RL", "RL", 
+Explanation: s can be split into "RL", "RRLL", "RL", "RL",
 each substring contains same number of 'L' and 'R'.
 
 Example 2:
 
 Input: s = "RLLLLRRRLR"
 Output: 3
-Explanation: s can be split into "RL", "LLLRRR", "LR", 
+Explanation: s can be split into "RL", "LLLRRR", "LR",
 each substring contains same number of 'L' and 'R'.
 
 Example 3:
@@ -46,6 +46,6 @@ Example 4:
 
 Input: s = "RLRRRLLRLL"
 Output: 2
-Explanation: s can be split into "RL", "RRRLLRLL", 
+Explanation: s can be split into "RL", "RRRLLRLL",
 since each substring contains an equal number of 'L' and 'R'
 """
